d5ffeb0b496b44b4c1d35f39fdfda2028c9603f6
Suporno <28supornomaiti@gmail.com>
2026-03-21 23:02:23 +0530

    scripts: Fixed setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,11 @@ set -e
 echo "Installing dependencies..."
 
 if command -v apt &> /dev/null; then
-    sudo apt install -y build-essential cmake libreadline-dev pkg-config
+    sudo apt install -y build-essential cmake libreadline-dev
 elif command -v pacman &> /dev/null; then
-    sudo pacman -S --noconfirm base-devel cmake readline pkg-config
+    sudo pacman -S --noconfirm base-devel cmake readline
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y gcc-c++ cmake readline-devel pkgconfig
+    sudo dnf install -y gcc-c++ cmake readline-devel
 else
     echo "Unknown package manager. Please install manually."
     exit 1
@@ -16,31 +16,6 @@ fi
 
 echo "Building..."
 rm -rf build
-sudo mkdir -p build
-cd build
-cmake ..
-make
-
-echo ""
-echo "Done! Run ./build/Shell_Project"
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-
-if command -v apt &> /dev/null; then
-    sudo apt install -y build-essential cmake libreadline-dev pkg-config
-elif command -v pacman &> /dev/null; then
-    sudo pacman -S --noconfirm base-devel cmake readline pkg-config
-elif command -v dnf &> /dev/null; then
-    sudo dnf install -y gcc-c++ cmake readline-devel pkgconfig
-else
-    echo "Unknown package manager. Please install manually."
-    exit 1
-fi
-
-echo "Building..."
-sudo rm -rf build
 mkdir -p build
 cd build
 cmake ..
